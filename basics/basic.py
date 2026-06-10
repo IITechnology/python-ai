b09e97eaa46630e7ab55e14fb8daa9708031c458
SonyDaman <ivaaninfotechsoultion@gmail.com>
2026-06-10 18:03:13 +0530

    Add calculator file

--- basics/basic.py
+++ basics/basic.py
@@ -4,3 +4,11 @@ print("Hello " + name)
 age = 30
 print("I am " + str(age) + " years old.")
 
+print(f"Name: {name}")
+print(f"Type of name: {type(name)}")  # Output: <class 'str'>
+print(f"Type of age: {type(age)}")  # Output: <class 'int'>
+
+isClassRunning = True
+print(f"isClassRunning: {isClassRunning}")
+print(f"Type of age: {type(isClassRunning)}")
+
---
+++ basics/calculator.py
@@ -0,0 +1,9 @@
+a = 10
+b = 15
+addtion = f"Addition : {a + b}"
+subtration = f"Subraction : {a - b}"
+multiplication = f"Multiplication : {a * b}"
+print(addtion)
+print(subtration)
+print(multiplication)
+# print(a / b)
