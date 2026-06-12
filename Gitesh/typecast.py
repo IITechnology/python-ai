882289f4d6219e82f36b373f79149aa22b5ca16b
git87-Tester <gitesh7891@gmail.com>
2026-06-12 17:46:05 +0530

    typecast changes

---
+++ Gitesh/typecast.py
@@ -0,0 +1,12 @@
+str_num="100"
+int_num=int(str_num)
+age=10
+
+#result=f"result:{str_num + age}"
+result1=f"result1:{int_num + age}"
+
+#print(result)
+print(result1)
+
+float_num=float(str_num)
+print(float_num)
