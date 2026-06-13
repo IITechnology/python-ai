2e549d853678fb1e27e7494af33872947c746bec
git87-Tester <gitesh7891@gmail.com>
2026-06-13 16:09:14 +0530

    inter prep

--- Gitesh/sample.py
+++ Gitesh/sample.py
@@ -3,4 +3,10 @@ students=["harjot","daksh","gitesh"]
 print(students)
 
 result=f"Student_List:{students}"
-students.append("vishal")
+students.append("vishal")\
+# Using f-string
+name="test"
+print(f"Welcome, {name}!")
+#interview question
+value = 42
+print(f"The answer is {{value}}: {value}")  
