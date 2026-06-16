7b1ca1f25b64ee9fe8ed5c4004c7d1c3414c6866
dakshpratapsingh16-ctrl <dakshpratapsingh16@gmail.com>
2026-06-16 06:39:18 +0530

    testing

---
+++ daksh/FUNCTIONS.py
@@ -0,0 +1,13 @@
+def greet():
+     print(f"hello world")
+     
+greet()
+greet()
+
+#function with simple parameters
+
+def user(user_name):
+     print(f"welcome {user_name}")
+     
+user("daksh")     
+user("harjot")
