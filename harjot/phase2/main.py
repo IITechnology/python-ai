779c0f8c3e536802cc71bf09da44bda16e1cb329
dakshpratapsingh16-ctrl <dakshpratapsingh16@gmail.com>
2026-06-25 18:16:55 +0530

    test

--- harjot/phase2/main.py
+++ harjot/phase2/main.py
@@ -9,3 +9,31 @@ def read_route():
         "phase": "21",
         "satus": "active"
     }
+    
+@app.get("/calculate/{num1}/{num2}")
+
+def calculate(num1:int,num2:int, opertaion: str="add"):
+        
+        if opertaion == "add":
+            return {
+                "result":num1 + num2
+    }
+        elif opertaion == "-":
+            return {
+                "result":num1 - num2
+    
+    }
+        elif opertaion == "*":
+
+            return {
+                "result":num1 * num2
+    }
+    
+        elif opertaion == "/":
+
+            return {
+                "result":num1 / num2
+            }
+        else:
+            print()  
+
